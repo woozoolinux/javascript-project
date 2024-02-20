684c6c6793d22351f431f0bb7d1d58074532bbb9
woozoolinux <magiccolor2@gmail.com>
2024-02-20 20:18:38 +0900

    change noona api

--- woozoo-times/main.js
+++ woozoo-times/main.js
@@ -1,11 +1,11 @@
 
-const API_KEY='021e05a5adf6492e929715a91c5e8765'
+//const API_KEY='021e05a5adf6492e929715a91c5e8765'
 let news=[];
 
 // 비동기 함수를 이용, await 사용할 때 필요
 const getLatestNews = async() => {
-    const url = new URL (`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
-    
+    //const url = new URL (`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
+    const url = new URL (`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`)
     //생성된 URL로 비동기 요청을 보낸다. await을 사용하면 응답이 올 때까지 기다린다.
     const response = await fetch(url);
     // 방금 받은 데이터를 JSON형식으로 요청(객체 처럼 생겨서 주고받고 사용하기가 편리)
